127d400460657a1db73517216d4998f0fe7251e5
Rupayan Dutta <102848153+Rupayan20@users.noreply.github.com>
2022-12-31 12:46:13 +0530

    Create Landing.js

---
+++ Holiday Packages/src/components/Pages/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../App.css'
+import Home from '../Home'
+
+function Landing () {
+    return (
+        <>
+        <Home />
+        
+        </>
+    )
+}
+export default Landing;
